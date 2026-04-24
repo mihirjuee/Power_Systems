72c9bae26b6c7f9ea2851f9119728ece9f7ca0f7
mihirjuee <mihir.juee@gmail.com>
2026-04-24 15:49:51 +0530

    Update trlinemodel.py

--- trlinemodel.py
+++ trlinemodel.py
@@ -93,7 +93,7 @@ def medium_line_circuit():
 
     d += elm.Line().right()
     #d += elm.Dot()
-    d += elm.Line().down(0.1)
+    d += elm.Line().down(0.8)
     d += elm.Resistor().label("Load")
     d += elm.Line().left(12)
     return d
