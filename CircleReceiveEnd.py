8a819c8df082afa02aee04d7bfb583fef667eeb1
mihirjuee <mihir.juee@gmail.com>
2026-05-02 21:24:53 +0530

    Update CircleReceiveEnd.py

--- CircleReceiveEnd.py
+++ CircleReceiveEnd.py
@@ -21,7 +21,7 @@ import matplotlib.pyplot as plt
 # ----------------------------------------------------------------------
 st.set_page_config(
     page_title="Receiving End Circle Diagram",
-    page_icon="⚡",
+    page_icon="logo.png",
     layout="wide"
 )
 
